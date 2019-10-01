2a6ff77b158bc2f447902204a4df39cfdcdf7c58
sun0511 <sun0511777@gmail.com>
2019-10-01 17:22:16 +0900

    done5

--- src/Routes/Auth/AuthQueries.js
+++ src/Routes/Auth/AuthQueries.js
@@ -9,15 +9,27 @@ export const LOG_IN = gql`
 export const CREATE_ACCOUNT = gql`
     mutation createAccount(
         $username: String!
-        $emai: String!
+        $email: String!
         $firstName: String
         $lastName: String
     ){
         createAccount(
             username: $username
-            emai: $email
+            email: $email
             firstName: $firstName
             lastName: $lastName
         )
     }
 `
+
+export const CONFIRM_SECRET = gql`
+  mutation confirmSecret($secret: String!, $email: String!) {
+    confirmSecret(secret: $secret, email: $email)
+  }
+`;
+
+export const LOCAL_LOG_IN = gql`
+  mutation logUserIn($token: String!) {
+    logUserIn(token: $token) @client
+  }
+`;

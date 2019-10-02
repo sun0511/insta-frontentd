a9b0655d6a119bf3791bbb46a2bfa260229227c2
sun0511 <sun0511777@gmail.com>
2019-10-02 17:17:21 +0900

    error

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,27 +1,48 @@
-import React from 'react';
-import { gql } from "apollo-boost"
-import {ThemeProvider} from "styled-components"
-import GlobalStyles from '../Styles/GlobalStyles';
-import Theme from '../Styles/Theme';
-import Router from "./Router"
-import { useQuery } from 'react-apollo-hooks';
-import Footer from './Footer';
+import React from "react";
+import { gql } from "apollo-boost";
+import styled, { ThemeProvider } from "styled-components";
+import { HashRouter as Router } from "react-router-dom";
+import { useQuery } from "react-apollo-hooks";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from "../Styles/Theme";
+import Routes from "./Routes";
+import Footer from "./Footer";
+import Header from "./Header";
 
-const QUERY = gql`{isLoggedIn @client}`
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
 
-export default () =>{
-    const { data:{isLoggedIn} } = useQuery(QUERY);
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: ${props => props.theme.maxWidth};
+  width: 100%;
+`;
 
-    return(
-        <ThemeProvider theme={Theme}>
-        <>
+export default () => {
+  const {
+    data: { isLoggedIn }
+  } = useQuery(QUERY);
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <>
         <GlobalStyles />
-        <Router isLoggedIn={isLoggedIn} />
-        <Footer/>
-        <ToastContainer position={toast.POSITION.BOTTOM_LEFT}/>
-        </>
-        </ThemeProvider>
-    )
-}
+        <Router>
+          <>
+            <Header />
+            <Wrapper>
+              <Routes isLoggedIn={isLoggedIn} />
+              <Footer />
+            </Wrapper>
+          </>
+        </Router>
+        <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
+      </>
+    </ThemeProvider>
+  );
+};
--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -1,13 +1,19 @@
 const BOX_BORDER = "1px solid #e6e6e6";
-const BORDER_RADIUS = "4px"
+const BORDER_RADIUS = "4px";
 
-export default{
-    bgColor: "#FAFAFA",
-    blackColor: "#262626",
-    darkGreyColor: "#999",
-    lightGreyColor:"#c7c7c7",
-    redColor: "#ED4956",
-    blueColor: "#3897f0",
-    darkBlueColor: "#003569",
-    whiteBox: `${BORDER_RADIUS};${BOX_BORDER};background-color:white`
-}
+export default {
+  maxWidth: "935px",
+  bgColor: "#FAFAFA",
+  blackColor: "#262626",
+  darkGreyColor: "#999",
+  lightGreyColor: "#c7c7c7",
+  redColor: "#ED4956",
+  blueColor: "#3897f0",
+  darkBlueColor: "#003569",
+  boxBorder: "1px solid #e6e6e6",
+  borderRadius: "4px",
+  whiteBox: `border:${BOX_BORDER};
+             border-radius:${BORDER_RADIUS};
+             background-color:white;
+            `
+};
